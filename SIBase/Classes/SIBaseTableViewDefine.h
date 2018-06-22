428730fe4950a7ad9aa175496f6fa5e5fd63cdca
ungacy <ungacy@126.com>
2018-06-22 10:37:07 +0800

    version bump to 0.1.30

--- SIBase/Classes/SIBaseTableViewDefine.h
+++ SIBase/Classes/SIBaseTableViewDefine.h
@@ -9,6 +9,9 @@
 #ifndef SIBaseTableViewDefine_h
 #define SIBaseTableViewDefine_h
 
+#import <SIDefine/SIDataBindDefine.h>
+#import "SIFormItem.h"
+
 typedef NSString * (^SIBaseTableCellReuseIdentifierBlock)(NSIndexPath *indexPath);
 
 typedef UITableViewCell * (^SIBaseTableCellCustomCellBlock)(NSIndexPath *indexPath, UITableViewCell *cell);
@@ -19,35 +22,7 @@ static NSString *const kSINoPermossionKey = @"NoPermossion";
 
 static const CGFloat kSIBaseTableViewDefaultCellHeight = 60;
 
-/**
- optional protocol
- */
-@protocol SIFormItemProtocol <NSObject>
 
-@optional
-@property (nonatomic, copy) NSString *key;
-
-@property (nonatomic, assign) CGFloat height;
-
-@property (nonatomic, assign) BOOL selected;
-
-@property (nonatomic, strong) id data;
-
-@property (nonatomic, copy) NSString *title;
-
-@property (nonatomic, copy) NSString *content;
-
-@property (nonatomic, copy) NSString *avatar;
-
-@property (nonatomic, copy) NSString *icon;
-
-@property (nonatomic, copy) NSString *identifier;
-
-@property (nonatomic, copy) NSDictionary *theme;
-
-@end
-
-#import "SIFormItem.h"
 
 static NSString *const kSIFormItemKeyTitle = @"title";
 
