950dc4d0d56ef004374ac1970c4cc2b647d03f10
ungacy <ungacy@126.com>
2018-08-29 09:57:45 +0800

    Revert "version bump to 0.1.58"
    This reverts commit fb608fa1529f48fcee365e057fab527ec33727af.

---
+++ SIBase/Classes/SIFormItem.m
@@ -0,0 +1,20 @@
+//
+//  SIFormItem.m
+//  SuperId
+//
+//  Created by Ye Tao on 2017/8/9.
+//  Copyright © 2017年 SuperId. All rights reserved.
+//
+
+#import "SIFormItem.h"
+
+@implementation SIFormItem
+
+- (BOOL)isEqual:(SIFormItem *)object {
+    if ([object isKindOfClass:[self class]]) {
+        return [self.id_p isEqualToNumber:object.id_p];
+    }
+    return NO;
+}
+
+@end
---
+++ SIBase/Classes/SIFormItemDefine.h
@@ -0,0 +1,50 @@
+//
+//  SIFormItemDefine.h
+//  SuperId
+//
+//  Created by Ye Tao on 2017/8/7.
+//  Copyright © 2017年 SuperId. All rights reserved.
+//
+
+#ifndef SIFormItemDefine_h
+#define SIFormItemDefine_h
+
+static NSString *const kSIFormItemThemeStar = @"star"; //default is `NO`
+
+static NSString *const kSIFormItemThemeLast = @"last"; //default is `NO`
+
+static NSString *const kSIFormItemThemeAvatarLeftOffset = @"avatarLeftOffset"; //default is `12`
+
+static NSString *const kSIFormItemThemeButtonLeftOffset = @"buttonLeftOffset"; //default is `12`
+
+static NSString *const kSIFormItemThemeTitleTopOffset = @"titleTopOffset"; //default is `18`
+
+static NSString *const kSIFormItemThemeTitleLeftOffset = @"titleLeftOffset"; //default is `12`
+
+static NSString *const kSIFormItemThemeTitleFont = @"titleFont"; //default is `12`
+
+static NSString *const kSIFormItemThemeContentFont = @"contentFont"; //default is `14`
+
+static NSString *const kSIFormItemThemeTitleColor = @"titleColor"; //default is `0x9b9b9b`
+
+static NSString *const kSIFormItemThemeContentColor = @"contentColor"; //default is `0x4a4a4a`
+
+static NSString *const kSIFormItemThemeContentHeight = @"contentHeight"; //default is `22`
+
+static NSString *const kSIFormItemThemeContentBottomOffset = @"contentBottomOffset"; //default is `-0.5`
+
+static NSString *const kSIFormItemThemeSwitch = @"switch"; //default is `NO`
+
+static NSString *const kSIFormItemThemeSwitchRightOffset = @"switchRightOffset"; //default is `40`
+
+static NSString *const kSIFormItemThemeContentRightOffset = @"contentRightOffset"; //default is `-34`
+
+static NSString *const kSIFormItemThemeCorner = @"corner"; //default is `0`
+
+static NSString *const kSIFormItemThemeUnit = @"unit";
+
+static NSString *const kSIFormItemThemeContentTextAlignment = @"contentTextAlignment"; //default is `NSTextAlignmentRight`
+
+static NSString *const kSIFormItemThemeContentClearButtonMode = @"contentClearButtonMode"; //default is `UITextFieldViewModeNever`
+
+#endif /* SIFormItemDefine_h */
